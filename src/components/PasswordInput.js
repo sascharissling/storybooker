c9cc9ba4f03b6b7bfd3afc38f4e863344a20b22c
sascharissling <sascha.rissling@gmail.com>
2019-12-10 14:58:29 +0100

    Solve aufgabe

--- src/components/PasswordInput.js
+++ src/components/PasswordInput.js
@@ -3,20 +3,42 @@ import styled from "@emotion/styled";
 
 //STYLE start
 
+function calcHue(props) {
+  return Math.min(props.value.length * 10, 100);
+}
+
 const Input = styled.input`
   width: 500px;
   height: 30px;
   font-size: 24px;
-  font-weight: bold;
+  border-radius: 15px;
   font-family: "Source Sans Pro", sans-serif;
+  background: red;
+  padding: 5px;
+  &:focus {
+    background: hsl(${calcHue}, 60%, 60%);
+    outline: 4px solid slateblue;
+  }
 `;
 
 //STYLE end
 
-export default function PasswordInput() {
+export default function PasswordInput({ onChange }) {
+  const [password, setPassword] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onChange(password);
+  }
+
+  function handleChange(event) {
+    const value = event.target.value;
+    setPassword(value);
+  }
+
   return (
-    <form>
-      <Input type="text" />
+    <form onChange={handleSubmit}>
+      <Input type="password" value={password} onChange={handleChange} />
     </form>
   );
 }
--- src/stories/2-PasswordInput.stories.js
+++ src/stories/2-PasswordInput.stories.js
@@ -5,18 +5,6 @@ import PasswordInput from "../components/PasswordInput";
 export default {
   title: "Password Input"
 };
-export function Weak() {
-  return <PasswordInput value="pw" />;
-}
-export function Multiple() {
-  return (
-    <>
-      <PasswordInput value="" onChange={action("change")} />
-      <PasswordInput value="pw" onChange={action("change")} />
-      <PasswordInput value="pass" onChange={action("change")} />
-      <PasswordInput value="password" onChange={action("change")} />
-      <PasswordInput value="passwordOver" onChange={action("change")} />
-      <PasswordInput value="passwordOver9000" onChange={action("change")} />
-    </>
-  );
+export function Password() {
+  return <PasswordInput />;
 }
